78a182bce00de166bdc1eaac3b60ea2983d537ba
TravisCI <travis@travis-ci.org>
2019-09-23 14:42:24 +0000

    deploy demo

---
+++ demo/build/precache-manifest.bb08f2cbdd7e9a4f09cd771cd246d21a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165b8cb644f87aa53dbf82b757de8a48",
+    "url": "/weather-forecast/demo/index.html"
+  },
+  {
+    "revision": "5524fc8f6b522a853981",
+    "url": "/weather-forecast/demo/static/css/main.4d9e4d01.chunk.css"
+  },
+  {
+    "revision": "8cefb8ba0cd1a46e52e4",
+    "url": "/weather-forecast/demo/static/js/2.7526df7c.chunk.js"
+  },
+  {
+    "revision": "5524fc8f6b522a853981",
+    "url": "/weather-forecast/demo/static/js/main.6e0997c9.chunk.js"
+  },
+  {
+    "revision": "2be13df7ac13b066d79d",
+    "url": "/weather-forecast/demo/static/js/runtime~main.f4e5d5ac.js"
+  }
+]);
