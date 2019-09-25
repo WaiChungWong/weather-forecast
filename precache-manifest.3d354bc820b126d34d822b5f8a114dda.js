ebfbba68c1a97622cb065b740faeed14adb6fe84
TravisCI <travis@travis-ci.org>
2019-09-25 13:05:45 +0000

    deploy website

---
+++ precache-manifest.3d354bc820b126d34d822b5f8a114dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c1b7d72be3d828acf446ca93778052",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "aa7d3bb94876c93e6dfc",
+    "url": "/weather-forecast/static/css/main.763332c7.chunk.css"
+  },
+  {
+    "revision": "69716a99fc399443bb36",
+    "url": "/weather-forecast/static/js/2.888e5fb5.chunk.js"
+  },
+  {
+    "revision": "aa7d3bb94876c93e6dfc",
+    "url": "/weather-forecast/static/js/main.64e94eff.chunk.js"
+  },
+  {
+    "revision": "1f4587d6beb10b2bf016",
+    "url": "/weather-forecast/static/js/runtime~main.96f59320.js"
+  }
+]);
